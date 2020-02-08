6912be0a5aad33a4f43fe09d440fddbc7ce78230
oral0717 <807910824@qq.com>
2020-02-08 23:04:56 +0800

    架构增加hot-module-replacement

--- Counter/client/app.js
+++ Counter/client/app.js
@@ -1,8 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { AppContainer } from 'react-hot-loader'
 import App from './App.jsx'
 
 // ReactDOM.render(<App/>, document.getElementById('root'))
-ReactDOM.hydrate(<App/>, document.getElementById('root'))
+// ReactDOM.hydrate(<App/>, document.getElementById('root'))
 
+const root = document.getElementById('root')
+const render = (Component)=>{
+  ReactDOM.render(
+    <AppContainer>
+      <Component />
+    </AppContainer>,
+    root
+  )
+}
+render(App)
 
+if (module.hot) {
+  module.hot.accept('./App.jsx', ()=>{
+    const NextApp = require('./App.jsx').default
+    render(NextApp)
+  })
+}
--- Counter/server/server.js
+++ Counter/server/server.js
@@ -14,7 +14,7 @@ app.use('/public', express.static(path.join(__dirname,'../dist')))
 // 从浏览器发出的任何请求，都让它返回服务端渲染的代码，服务端代码在server-entry.js里，所有需要引入它
 app.get('*', function(req,res){
   const appString = ReactSSR.renderToString(serverEnter)
-  res.send(template.replace('<!--app-->', appString))
+  res.send(template.replace('<!-- app -->', appString))
 })
 
 // 回调函数报告启动服务成功，启动命令放入package.json里
