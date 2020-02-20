ee1da20b0c31bc834d300e554104b16a825e4353
oral0717 <807910824@qq.com>
2020-02-20 15:30:39 +0800

    todolist功能

---
+++ todolist/src/TodoItem.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+export default class TodoItem extends Component{
+  constructor(props){
+    super(props)
+    this.handleClick = this.handleClick.bind(this)
+  }
+  static propTypes = {
+    item: PropTypes.string,
+    index: PropTypes.number,
+    onHandleDel: PropTypes.func,
+    test: PropTypes.string
+  }
+  static defaultProps = {
+    test: 'xx'
+  }
+
+  render(){
+    const { item, test} = this.props
+    return (
+      <li onClick={this.handleClick}>{test} - {item}</li>
+    )
+  }
+
+  handleClick(){
+    const { onHandleDel, index } = this.props
+    onHandleDel(index)
+  }
+}
---
+++ todolist/src/TodoList.js
@@ -0,0 +1,79 @@
+import React, { Component, Fragment } from 'react'
+import TodoItem from './TodoItem'
+import './style.css'
+
+class TodoList extends Component{
+  constructor(){
+    super()
+    this.state = {
+      inputValue: '',
+      list: []
+    }
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+    this.handleDel = this.handleDel.bind(this)
+  }
+
+  render(){
+    const { inputValue } = this.state
+    return (
+      <Fragment>
+        <div>
+          <input
+            className='input'
+            type='text'
+            value={inputValue}
+            onChange={this.handleChange} />
+          <button
+            onClick={this.handleSubmit}>提交</button>
+        </div>
+        <ul>
+          {
+            this.getTodoItem()
+          }
+        </ul>
+      </Fragment>
+    )
+  }
+  getTodoItem(){
+    const { list } = this.state
+    return  list.map((item, index)=>{
+      return (
+        <TodoItem
+          key={index}
+          index={index}
+          item={item}
+          onHandleDel={this.handleDel}/>
+      )
+    })
+  }
+  handleChange(e){
+    const {value} = e.target
+    this.setState(() => {
+      return {
+        inputValue: value
+      }
+    })
+  }
+
+  handleSubmit(){
+    this.setState((prevState)=>{
+      const { inputValue, list } = prevState
+      return {
+        list: [...list, inputValue],
+        inputValue: ''
+      }
+    })
+  }
+
+  handleDel(index){
+    this.setState((prevState)=>{
+      const { list } = prevState
+      list.splice(index, 1)
+      return {
+        list: [...list]
+      }
+    })
+  }
+}
+export default TodoList
