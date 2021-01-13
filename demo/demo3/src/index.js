9f19a0b3cca6f225770b02c76afdc1ba230fcfef
oral0717 <807910824@qq.com>
2021-01-13 15:53:15 +0800

    update

--- demo/demo3/src/index.js
+++ demo/demo3/src/index.js
@@ -1,9 +1,11 @@
 import _ from 'lodash'
+import './style.css'
 
 function component() {
   const element = document.createElement('div')
   // lodash，现在通过一个 script 引入
   element.innerHTML = _.join(['Hello', 'webpack'], ' ')
+  element.classList.add('hello');
   return element
 }
 
