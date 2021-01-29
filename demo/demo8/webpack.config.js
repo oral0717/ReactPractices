c32085360f0b5c9aeb8ba33c2cf35e2be45ac52a
oral0717 <807910824@qq.com>
2021-01-29 15:50:37 +0800

    update

---
+++ demo/demo8/webpack.config.js
@@ -0,0 +1,60 @@
+const path = require('path');
+const webpack = require('webpack');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+
+
+
+/*
+ * We've enabled MiniCssExtractPlugin for you. This allows your app to
+ * use css modules that will be moved into a separate CSS file instead of inside
+ * one of your module entries!
+ *
+ * https://github.com/webpack-contrib/mini-css-extract-plugin
+ *
+ */
+
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+
+
+
+module.exports = {
+  mode: 'development',
+  entry: './src/scripts/index.js',
+
+  plugins: [
+    new webpack.ProgressPlugin(),
+    new CleanWebpackPlugin(), // 每次编译删除dist
+    new MiniCssExtractPlugin({ filename:'style.[contenthash].css' })
+  ],
+  output: {
+    filename: '[name].[contenthash].js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [{
+      test: /\.(js|jsx)$/,
+      include: [path.resolve(__dirname, 'src/scripts')],
+      loader: 'babel-loader'
+    }, {
+      test: /.(sa|sc|c)ss$/,
+
+      use: [{
+        loader: MiniCssExtractPlugin.loader
+      }, {
+        loader: "css-loader",
+
+        options: {
+          sourceMap: true
+        }
+      }, {
+        loader: "sass-loader",
+
+        options: {
+          sourceMap: true
+        }
+      }]
+    }]
+  }
+}
