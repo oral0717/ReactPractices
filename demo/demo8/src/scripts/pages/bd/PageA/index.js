c528db7894c22cb63ca81e30aae0ec78c252fe26
oral0717 <807910824@qq.com>
2021-02-19 15:29:50 +0800

    useState

--- demo/demo8/src/scripts/pages/bd/PageA/index.js
+++ demo/demo8/src/scripts/pages/bd/PageA/index.js
@@ -2,8 +2,29 @@ import React, { useState } from 'react'
 import { Link } from "react-router-dom";
 import { Button } from 'antd'
 
-const PageA = () => {
+function PageA () {
   const [count, setCount] = useState(1)
+  const [user, setUser] = useState({
+    name: 'zz',
+    age: 18
+  })
+
+  const handleClick = ({name}) => {
+    // console.log(111, this, setCount)
+    if (name === 'count') {
+      setCount(count + 1)
+      console.log(count, name)
+    }
+    if (name === 'update') {
+      const newAge = user.age + 2
+      setUser({
+        ...user,
+        name: 'jj',
+        age: newAge
+      })
+      console.log(user, name)
+    }
+  }
 
   return (
     <div>
@@ -20,7 +41,14 @@ const PageA = () => {
           </li>
       </ul>
       <p>Page A</p>
-      <p>累加器：{count}<Button type="primary"> + </Button></p>
+      <p>
+        累加器：{count}
+        <Button type="primary" onClick={() => handleClick({name: 'count'})}> + </Button>
+      </p>
+      <p>
+        设置用户：{'姓名：' + user.name + ' 年龄：' + user.age}
+        <Button type="primary" danger onClick={handleClick.bind(null,{name: 'update'})}> 更改用户信息 </Button>
+      </p>
     </div>
   )
 }
