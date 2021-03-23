c9fd5b7824d205021c31d67c1588ac2959c8102d
oral0717 <807910824@qq.com>
2021-03-23 17:39:07 +0800

    update

--- microFrontend/microFrontendDemo3/main-app/src/pages/index.tsx
+++ microFrontend/microFrontendDemo3/main-app/src/pages/index.tsx
@@ -1,9 +1,22 @@
-import styles from './index.less';
+import { Layout, Menu } from 'antd';
+const { Header, Content, Footer } = Layout;
+import './index.less';
 
 export default function IndexPage() {
   return (
-    <div>
-      <h1 className={styles.title}>Page index</h1>
-    </div>
+    <Layout className="layout">
+      <Header>
+        <div className="logo" >微前端</div>
+        <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['3']}>
+          <Menu.Item key="1">app 1</Menu.Item>
+          <Menu.Item key="2">app 2</Menu.Item>
+          <Menu.Item key="3">app 3</Menu.Item>
+        </Menu>
+      </Header>
+      <Content>
+        <div className="site-layout-content">Content</div>
+      </Content>
+      <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+    </Layout>
   );
 }
