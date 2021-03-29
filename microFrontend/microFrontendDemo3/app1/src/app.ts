10abd241cdd0e129b40581a12f7f87c369a513ae
oral0717 <807910824@qq.com>
2021-03-29 18:51:18 +0800

    update

--- microFrontend/microFrontendDemo3/app1/src/app.ts
+++ microFrontend/microFrontendDemo3/app1/src/app.ts
@@ -2,15 +2,15 @@
 // src/app.ts
 export const qiankun = {
   // 应用加载之前
-  async bootstrap(props) {
+  async bootstrap(props: any) {
     console.log('app1 bootstrap', props);
   },
   // 应用 render 之前触发
-  async mount(props) {
+  async mount(props: any) {
     console.log('app1 mount', props);
   },
   // 应用卸载之后触发
-  async unmount(props) {
+  async unmount(props: any) {
     console.log('app1 unmount', props);
   },
 };
