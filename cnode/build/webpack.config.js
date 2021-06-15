cf31cb14ec758f85902a91f3b584b52ee267d868
oral0717 <807910824@qq.com>
2021-06-16 00:10:13 +0800

    update

--- cnode/build/webpack.config.js
+++ cnode/build/webpack.config.js
@@ -1,12 +1,27 @@
 const path = require('path')
+const HTMLPlugin = require('html-webpack-plugin')
 
 module.exports = {
   entry: {
-    app: path.join(__dirname, '../client/app.js')
+    app: path.join(__dirname, '../client/index.js')
   },
   output: {
     filename: '[name].[hash].js',
     path: path.join(__dirname, '../dist'),
     publicPath: '/public/'
-  }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js|jsx$/,
+        loader: 'babel-loader',
+        exclude: [
+          path.join(__dirname, '../node_modules')
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HTMLPlugin()
+  ]
 }
